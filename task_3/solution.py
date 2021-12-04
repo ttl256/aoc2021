268735ceb80c01836fb4b47a9ea3e4ff85ef0c4d
ttl256 <64251319+ttl256@users.noreply.github.com>
2021-12-04 18:57:54 +0300

    Task 3 part 2

--- task_3/solution.py
+++ task_3/solution.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict
+from pprint import pprint
 
 
 def parse(x):
@@ -26,6 +27,22 @@ def build_data(x):
     return d
 
 
+def sieve(x, bit_pos=0, bit_keep=None):
+    if len(x) == 1:
+        return x[0]
+    t = {0: min, 1: max}
+    pick = t[bit_keep]
+    data = build_data(x)
+    cur_bit = data[bit_pos]
+    # Check for equal number of 0 and 1
+    if len(set(cur_bit.values())) == 1:
+        common_bit = bit_keep
+    else:
+        common_bit = pick(cur_bit, key=cur_bit.get)
+    new_list = [i for i in x if i[bit_pos] == common_bit]
+    return sieve(new_list, bit_pos=bit_pos + 1, bit_keep=bit_keep)
+
+
 def main(filename):
     with open(filename) as f:
         data = build_data(parse(f))
@@ -45,6 +62,18 @@ def main(filename):
         print(f"{gamma_rate=}, {gamma_rate_int=}")
         print(f"{epsilon_rate=}, {epsilon_rate_int=}")
         print(gamma_rate_int * epsilon_rate_int)
+
+    with open(filename) as f:
+        a = list(parse(f))
+        res1 = sieve(a, bit_pos=0, bit_keep=1)
+        print(res1)
+        res2 = sieve(a, bit_pos=0, bit_keep=0)
+        print(res2)
+        res1_int = int("".join(map(str, res1)), 2)
+        print(res1_int)
+        res2_int = int("".join(map(str, res2)), 2)
+        print(res2_int)
+        print(res1_int * res2_int)
 
 
 if __name__ == "__main__":
