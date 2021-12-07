363557e2713dbda792472bec27277fb9c1e13ca6
ttl256 <64251319+ttl256@users.noreply.github.com>
2021-12-07 03:00:18 +0300

    Refactor task 5

--- task5/solution.py
+++ task5/solution.py
@@ -1,7 +1,8 @@
 import sys
-from dataclasses import dataclass
+from pprint import pprint
 from itertools import repeat
 from collections import namedtuple
+from collections import defaultdict
 
 
 Point = namedtuple("Point", "x y")
@@ -16,23 +17,26 @@ class Vent:
     def get_span(self):
         end, start = self.coords
         diff = Point(x=end.x - start.x, y=end.y - start.y)
-        if diff.x == 0:
-            ax = repeat(0)
+        if max(abs(diff.x), abs(diff.y)) == 0:
+            span = self.coords
         else:
-            ax = (1 if diff.x > 0 else -1 for i in range(abs(diff.x)))
-        if diff.y == 0:
-            ay = repeat(0)
-        else:
-            ay = (1 if diff.y > 0 else -1 for i in range(abs(diff.y)))
-        inc_vector = zip(ax, ay)
-        span = []
-        span.append(start)
-        bx = start.x
-        by = start.y
-        for ix, iy in inc_vector:
-            bx += ix
-            by += iy
-            span.append(Point(x=bx, y=by))
+            dx = (
+                repeat(0)
+                if diff.x == 0
+                else (1 if diff.x > 0 else -1 for i in range(abs(diff.x)))
+            )
+            dy = (
+                repeat(0)
+                if diff.y == 0
+                else (1 if diff.y > 0 else -1 for i in range(abs(diff.y)))
+            )
+            span = []
+            span.append(start)
+            sx, sy = start.x, start.y
+            for ix, iy in zip(dx, dy):
+                sx += ix
+                sy += iy
+                span.append(Point(sx, sy))
 
         return span
 
@@ -43,28 +47,16 @@ def parse(x):
         yield ps
 
 
-def max_val(x):
-    res = 0
-    for i in x:
-        t = max(max(i))
-        if t > res:
-            res = t
-    return res
-
-
 def main(filename):
     with open(filename) as f:
-        vents_raw = list(parse(f))
-    l = max_val(vents_raw)
-    # st_p = [[a, b] for a, b in vents_raw if a.x == b.x or a.y == b.y]
-    grid = [0 for i in range((l + 1) * (l + 1))]
-    vents = [Vent(i) for i in vents_raw]
-    a = [i.get_span() for i in vents]
-    for vent in a:
-        for p in vent:
-            grid[l * p.y + p.x] += 1
-    res = sum(1 for i in grid if i >= 2)
-    print(res)
+        vents_raw = parse(f)
+        spans = (Vent(i).get_span() for i in vents_raw)
+        d = defaultdict(int)
+        for span in spans:
+            for point in span:
+                d[point] += 1
+
+    print(sum(1 for v in d.values() if v >= 2))
 
 
 if __name__ == "__main__":
